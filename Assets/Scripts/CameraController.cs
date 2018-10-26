831034f6611001767219bda1fc004cd1363b83a1
taylorjames9@gmail.com <taylorjames9@gmail.com>
2018-10-26 12:54:00 -0700

    character working + environment setup

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 1. Follow on player's X/Z plane
+/// 2. Smooth rotations around the player in 45 degree increments
+/// </summary>
+public class CameraController : MonoBehaviour {
+
+	public Transform target;
+	public Vector3 offsetPos;
+	public float moveSpeed = 5;
+	public float turnSpeed = 10;
+	public float smoothSpeed = 0.5f;
+
+	Quaternion targetRotation;
+	Vector3 targetPos;
+	bool smoothRotating = false;
+
+	void LateUpdate() {
+		MoveWithTarget();
+		LookAtTarget();
+
+		if (Input.GetKeyDown(KeyCode.G) && !smoothRotating) {
+			StartCoroutine("RotateAroundTarget", 45);
+		}	
+
+		if (Input.GetKeyDown(KeyCode.H) && !smoothRotating) {
+			StartCoroutine("RotateAroundTarget", -45);
+		}
+	}
+
+	/// <summary>
+	/// Move the camera to the player position + current camera offset
+	/// Offset is modified by the RotateARoundTarget coroutine
+	/// </summary>
+	void MoveWithTarget() {
+		targetPos = target.position + offsetPos;
+		transform.position = Vector3.Lerp(transform.position, targetPos, moveSpeed * Time.deltaTime);
+	}
+
+	/// <summary>
+	/// Use the look vector (target - current) to aim the camera toward the player
+	/// </summary>
+	void LookAtTarget() {
+		targetRotation = Quaternion.LookRotation(target.position - transform.position);
+		transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
+	}
+
+	/// <summary>
+	/// This coroutine can only have one instance running at a time
+	/// Determined by 'smoothRotating'
+	/// </summary>
+	IEnumerator RotateAroundTarget(float angle) {
+		Vector3 vel = Vector3.zero;
+		Vector3 targetOffsetPos = Quaternion.Euler(0, angle, 0) * offsetPos;
+		float dist = Vector3.Distance(offsetPos, targetOffsetPos);
+		smoothRotating = true;
+		
+		while (dist > 0.02f) {
+			offsetPos = Vector3.SmoothDamp(offsetPos, targetOffsetPos, ref vel, smoothSpeed);
+			dist = Vector3.Distance(offsetPos, targetOffsetPos);
+			yield return null;
+		}
+
+		smoothRotating = false;
+		offsetPos = targetOffsetPos;
+	}
+}
---
+++ Assets/Scripts/MarioController.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 1. 8-directional movement
+/// 2. stop and face current direction when input is absent
+/// </summary>
+public class MarioController : MonoBehaviour {
+
+	public float velocity = 5;
+	public float turnSpeed = 10;
+
+	Vector2 input;
+	float angle;
+
+  bool jumping;
+
+	Quaternion targetRotation;
+	Transform cam;
+  Animator anim;
+
+	void Start() {
+		cam = Camera.main.transform;
+    anim = GetComponent<Animator>();
+	}
+
+	void Update() {
+		GetInput();
+
+		if (Mathf.Abs(input.x) < 1 && Mathf.Abs(input.y) < 1) return;
+
+		CalculateDirection();
+		Rotate();
+		Move();
+
+    if (Input.GetKeyDown(KeyCode.Space))
+    {
+      Jump();
+    }
+
+	}
+
+  private void FixedUpdate()
+  {
+
+  }
+
+  /// <summary>
+  /// Input based on Horizontal(a,d,<,>) and Vertical (w,s,^,v) keys
+  /// </summary>
+  void GetInput() {
+		input.x = Input.GetAxis("Horizontal");
+    input.y = Input.GetAxis("Vertical");
+
+    anim.SetFloat("BlendX", input.x);
+    anim.SetFloat("BlendY", input.y);
+	}
+
+	/// <summary>
+	/// Direction relative to the camera's rotation
+	/// </summary>
+	void CalculateDirection() {
+		angle = Mathf.Atan2(input.x, input.y);
+		angle = Mathf.Rad2Deg * angle;
+		angle += cam.eulerAngles.y;
+	}
+
+	/// <summary>
+	///	Rotate toward the calculated angle
+	/// </summary>
+	void Rotate() {
+		targetRotation = Quaternion.Euler(0, angle, 0);
+		transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
+	}
+
+	/// <summary>
+	/// This player only moves along its own forward axis
+	/// </summary>
+	void Move() {
+		transform.position += transform.forward * velocity * Time.deltaTime;
+	}
+
+  void Jump(){
+    //transform.position += transform.up * (200*Time.deltaTime);
+    GetComponent<Rigidbody>().AddForce(new Vector3(0,6,0), ForceMode.Impulse);
+    anim.SetTrigger("jump");
+    Debug.Log("Jump Fired");
+  }
+
+  private void OnCollisionEnter(Collision collision)
+  {
+    if(collision.transform.tag == "cube"){
+      anim.SetTrigger("FallBack");
+      //Debug.Break();
+    }
+  }
+
+}
